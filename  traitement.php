67fddffc74e7815bd3e2967fa60150828bf02f95
svenisis1 <kazumasis2001@gmail.com>
2015-12-16 15:05:27 +0000

    ajout

---
+++  traitement.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>                  
+<html>
+    <head>        
+                        
+        <meta charset="utf-8"/>   
+        
+        <!--S'assure que EI est à jour-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <!--Rendu sans zoom sur les appareil mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <meta name="description" content="CV">
+        <meta name="author" content="Michel valérian">
+        
+        <!--link vers bootstrap-->
+        <link href="css/bootstrap.css" rel="stylesheet" >
+        
+        
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+      
+      
+<?php
+if /*Un petit test pour savoir si le formulaire est envoyé*/ {
+    echo '
+    <script type="text/javascript">
+    alert("formulaire envoyé");
+    </script>';
+}
